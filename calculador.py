914ed7ecb2e53a419a92bf46edac2be539959fb6
NeroIvi <ivanreollo10@gmail.com>
2024-05-23 17:11:02 +0200

    cc

--- calculador.py
+++ calculador.py
@@ -1,14 +1,23 @@
-import tkinter as tk
+# import tkinter as tk
 
-pantalla = tk.Tk()
-pantalla.title("Calculadora en python")
+# pantalla = tk.Tk()
+# pantalla.title("Calculadora en python")
 
-b1 = tk.Button(pantalla, text ="1",height= 5, width=10)
-b1.grid(row=0, column=1)
-
-b2 = tk.Button(pantalla, text ="2",height= 5, width=10)
-b2.grid(row=0, column=2)
-pantalla.mainloop() 
-
+# b1 = tk.Button(pantalla, text ="1",height= 5, width=10)
+# b2 = tk.Button(pantalla, text ="2",height= 5, width=10)
+# b1.grid(row = 0, column = 0, pady = 2)
+# b2.grid(row = 1, column = 0, pady = 2)
+# pantalla.geometry("700x700+100+100")
+# pantalla.mainloop() 
+from tkinter import *
+root = Tk( )
+b=0
+for r in range(1, 6):
+    for c in range(6):
+        b=b+1
+        Button(root, text=str(b),
+        borderwidth=1 ).grid(row=r,column=c)
+pantalla = Label(root, text="A").grid(row=0, column=0, columnspan=6)
+root.mainloop()
 
 
