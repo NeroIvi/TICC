b1d90a2e5b200340dea5fc838c2aab467c206115
NeroIvi <ivanreollo10@gmail.com>
2024-05-23 17:02:58 +0200

    tama

--- calculador.py
+++ calculador.py
@@ -3,13 +3,12 @@ import tkinter as tk
 pantalla = tk.Tk()
 pantalla.title("Calculadora en python")
 
-b1 = tk.Button(pantalla, text ="1")
+b1 = tk.Button(pantalla, text ="1",height= 5, width=10)
 b1.grid(row=0, column=1)
+
+b2 = tk.Button(pantalla, text ="2",height= 5, width=10)
+b2.grid(row=0, column=2)
 pantalla.mainloop() 
 
 
-resultado = tk.Tk()
-resultado.grid(row = 0, column =0)
 
-pantalla.title("Mi pantalla en blanco")
-pantalla.mainloop()
